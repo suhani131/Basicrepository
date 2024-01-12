394c9865933a4c6de7d52b323ac6d0ff1a72f037
suhani131 <suhaninimje16@gmail.com>
2024-01-12 05:33:22 -0800

    change operators

--- operators.js
+++ operators.js
@@ -1,13 +1,13 @@
 let name='Heena';
 let name1="suhani";
-let name2 ="kunal";
+let name2 ="komal";
 if(name=='Heena' && name1=='Suhani'){
 console.log("We can Start Batch")
 }
 else{
     console.log("Provide Holiday");
 }
-if(name2!='kunal'){
+if(name2!='komal'){
     console.log("Contact Admin team")  
 }
 else{
